ae70e1514fcc8e41c2e61f0710afcfb6528e0eec
Sergey <redmoloch@yanadex.ru>
2021-03-15 19:44:09 +0300

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,16 @@ module.exports = {
           {
             test: /\.css$/i,
             use: [MiniCssExtractPlugin.loader, 'css-loader']
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
           }
         ]
       },
